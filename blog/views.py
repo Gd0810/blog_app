49e84de70c12857346a6a835d4e3d3701c79bf6c
Gd0810 <zeefer2000@gmail.com>
2026-04-28 21:55:35 +0530

    update:old_files

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.decorators import login_required
 from .models import Blog, Category, Comment, UserProfile
 from .forms import CustomUserCreationForm, CustomAuthenticationForm, BlogForm, CommentForm, UserProfileForm
 from django.contrib import messages
+import logging
+
+logger = logging.getLogger(__name__)
 
 def home(request):
     blogs = Blog.objects.all().order_by('-published_date')
@@ -42,34 +45,53 @@ def account_settings(request):
     update_form = BlogForm()
 
     if request.method == 'POST':
+        logger.info(
+            "account_settings POST user_id=%s username=%s action_keys=%s file_keys=%s",
+            request.user.id,
+            request.user.username,
+            [k for k in request.POST.keys() if k != 'csrfmiddlewaretoken'],
+            list(request.FILES.keys()),
+        )
         if 'blog_submit' in request.POST:
             blog_form = BlogForm(request.POST, request.FILES)
             if blog_form.is_valid():
                 blog = blog_form.save(commit=False)
                 blog.author = request.user
                 blog.save()
+                logger.info("Blog created user_id=%s blog_id=%s", request.user.id, blog.id)
                 messages.success(request, 'Blog created successfully!')
                 return redirect('account_settings')
+            logger.warning("Blog create failed user_id=%s errors=%s", request.user.id, blog_form.errors.as_json())
+            messages.error(request, f'Could not create blog. Errors: {blog_form.errors.as_text()}')
         elif 'profile_submit' in request.POST:
             profile_form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
             if profile_form.is_valid():
                 profile_form.save()
+                logger.info("Profile updated user_id=%s has_picture=%s", request.user.id, bool(user_profile.profile_picture))
                 messages.success(request, 'Profile updated successfully!')
                 return redirect('account_settings')
+            logger.warning("Profile update failed user_id=%s errors=%s", request.user.id, profile_form.errors.as_json())
+            messages.error(request, f'Could not update profile. Errors: {profile_form.errors.as_text()}')
         elif 'update_blog' in request.POST:
             blog_id = request.POST.get('blog_id')
             blog = get_object_or_404(Blog, id=blog_id, author=request.user)
             update_form = BlogForm(request.POST, request.FILES, instance=blog)
             if update_form.is_valid():
                 update_form.save()
+                logger.info("Blog updated user_id=%s blog_id=%s", request.user.id, blog.id)
                 messages.success(request, 'Blog updated successfully!')
                 return redirect('account_settings')
+            logger.warning("Blog update failed user_id=%s blog_id=%s errors=%s", request.user.id, blog_id, update_form.errors.as_json())
+            messages.error(request, f'Could not update blog. Errors: {update_form.errors.as_text()}')
         elif 'delete_blog' in request.POST:
             blog_id = request.POST.get('blog_id')
             blog = get_object_or_404(Blog, id=blog_id, author=request.user)
             blog.delete()
+            logger.info("Blog deleted user_id=%s blog_id=%s", request.user.id, blog_id)
             messages.success(request, 'Blog deleted successfully!')
             return redirect('account_settings')
+        else:
+            logger.warning("account_settings unknown POST action user_id=%s keys=%s", request.user.id, list(request.POST.keys()))
 
     return render(request, 'blog/account_settings.html', {
         'user_profile': user_profile,
